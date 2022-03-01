895c92aec385f086e2181f253b771044207c8ec9
jafarpagerjaya <jafarpager@gmail.com>
2022-03-01 09:19:06 +0700

    menambahkan file unload-img-handleing.js dan menyematkannya pada thamplate route, menambahkan span pada lits tb.html

---
+++ public/assets/main/js/unload-img-handling.js
@@ -0,0 +1,21 @@
+const imgOnErrorList = document.querySelectorAll('img[alt]');
+// let defaultBrokenSrcImg = 'https://secure.gravatar.com/avatar?d=wavatar';
+
+imgOnErrorList.forEach(errorImg => {
+    let isLoaded = errorImg.naturalHeight !== 0;
+
+    if (isLoaded) {
+        return;
+    }
+
+    if (typeof defaultBrokenSrcImg == 'string') {
+        errorImg.src = defaultBrokenSrcImg;
+        return;
+    }
+
+    let altValue = '(Gambar Rusak) '+errorImg.getAttribute('alt');
+
+    alt = document.createTextNode(altValue);
+    errorImg.parentNode.insertBefore(alt,errorImg);
+    errorImg.parentNode.removeChild(errorImg);
+});
