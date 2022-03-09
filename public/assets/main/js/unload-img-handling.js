05b23dcbcc278b73705f717d52130c3f16704855
jafarpagerjaya <jafarpager@gmail.com>
2022-03-09 12:45:45 +0700

    mengubah file unload-img-handling dengan membungkus isi fungsi kedalam setTimeout()

--- public/assets/main/js/unload-img-handling.js
+++ public/assets/main/js/unload-img-handling.js
@@ -2,20 +2,22 @@ const imgOnErrorList = document.querySelectorAll('img[alt]');
 // let defaultBrokenSrcImg = 'https://secure.gravatar.com/avatar?d=wavatar';
 
 imgOnErrorList.forEach(errorImg => {
-    let isLoaded = errorImg.naturalHeight !== 0;
+    setTimeout(() => {
+        let isLoaded = errorImg.naturalHeight !== 0;
 
-    if (isLoaded) {
-        return;
-    }
+        if (isLoaded) {
+            return;
+        }
 
-    if (typeof defaultBrokenSrcImg == 'string') {
-        errorImg.src = defaultBrokenSrcImg;
-        return;
-    }
+        if (typeof defaultBrokenSrcImg == 'string') {
+            errorImg.src = defaultBrokenSrcImg;
+            return;
+        }
 
-    let altValue = '(Gambar Rusak) '+errorImg.getAttribute('alt');
+        let altValue = '(Gambar Rusak) '+errorImg.getAttribute('alt');
 
-    alt = document.createTextNode(altValue);
-    errorImg.parentNode.insertBefore(alt,errorImg);
-    errorImg.parentNode.removeChild(errorImg);
+        alt = document.createTextNode(altValue);
+        errorImg.parentNode.insertBefore(alt,errorImg);
+        errorImg.parentNode.removeChild(errorImg);
+    });
 });
