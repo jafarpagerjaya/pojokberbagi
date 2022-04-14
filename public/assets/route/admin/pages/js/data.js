b57ce9d3ac7e25347af5a6b39ba6a987ac37c9c8
jafarpagerjaya <jafarpager@gmail.com>
2022-04-14 15:35:15 +0700

    membuat file data-chart.js dan memindahkan script untuk chart ke situ

--- public/assets/route/admin/pages/js/data.js
+++ public/assets/route/admin/pages/js/data.js
@@ -155,36 +155,3 @@ $('input[name="search"]').on('keyup', function() {
         fetchData('/admin/fetch/read/donasi/bantuan', data, root);
     }, 1000);
 });
-
-var ctx = document.getElementById("myChart");
-
-var data = {
-    labels: ["Agus","Sept","Octo"],
-    datasets: [
-        {
-            label: "Donasi ",
-            fill: false,
-            lineTension: 0.1,
-            backgroundColor: "rgba(75,192,192,0.4)",
-            borderColor: "rgba(75,192,192,1)",
-            borderCapStyle: 'butt',
-            borderJoinStyle: 'miter',
-            borderWidth: 1,
-            pointBorderColor: "rgba(75,192,192,1)",
-            pointBackgroundColor: "#fff",
-            pointBorderWidth: 5,
-            pointHoverRadius: 10,
-            pointHoverBackgroundColor: "rgba(75,192,192,1)",
-            pointHoverBorderColor: "rgba(220,220,220,1)",
-            pointHoverBorderWidth: 5,
-            pointRadius: 5,
-            pointHitRadius: 10,
-            data: [1, 3, 2],
-        }
-    ]
-};
-
-var myLineChart = new Chart(ctx, {
-    type: 'line',
-    data: data,
-});
